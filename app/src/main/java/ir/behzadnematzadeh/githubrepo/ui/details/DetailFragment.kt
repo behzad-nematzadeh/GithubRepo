d4a7e58ba7738a40aba7b55d2aa2eef2f86ad94a
Behzad Nematzadeh <behzad.nematzadeh@yahoo.com>
2022-05-04 16:54:51 +0430

    read nav-args

--- app/src/main/java/ir/behzadnematzadeh/githubrepo/ui/details/DetailFragment.kt
+++ app/src/main/java/ir/behzadnematzadeh/githubrepo/ui/details/DetailFragment.kt
@@ -1,7 +1,8 @@
 package ir.behzadnematzadeh.githubrepo.ui.details
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,10 +10,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import ir.behzadnematzadeh.githubrepo.BaseApp
-import ir.behzadnematzadeh.githubrepo.R
 import ir.behzadnematzadeh.githubrepo.databinding.FragmentDetailsBinding
-import ir.behzadnematzadeh.githubrepo.databinding.FragmentMainBinding
 import javax.inject.Inject
 
 class DetailFragment : Fragment() {
@@ -20,6 +20,8 @@ class DetailFragment : Fragment() {
     private var _binding: FragmentDetailsBinding? = null
     private val binding: FragmentDetailsBinding
         get() = requireNotNull(_binding) { "You can't touch binding when it's null" }
+
+    private val args by navArgs<DetailFragmentArgs>()
 
     @Inject
     lateinit var viewModelFactory: DetailViewModel.Factory
@@ -46,7 +48,14 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnBack.setOnClickListener{
+        binding.tvTitle.text = args.repository.name
+        binding.aDetailsTxt.text = args.repository.url
+
+        binding.aDetailsBtn.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(args.repository.url)))
+        }
+
+        binding.btnBack.setOnClickListener {
             findNavController().popBackStack()
         }
     }
