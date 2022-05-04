bd3dfbf459e5fae12a14b6f5dd0a6bd424e5ba9e
Behzad Nematzadeh <behzad.nematzadeh@yahoo.com>
2022-05-04 13:33:24 +0430

    improve MainActivity.kt

--- app/src/main/java/ir/behzadnematzadeh/githubrepo/ui/MainActivity.kt
+++ app/src/main/java/ir/behzadnematzadeh/githubrepo/ui/MainActivity.kt
@@ -6,17 +6,27 @@ import androidx.navigation.fragment.NavHostFragment
 import ir.behzadnematzadeh.githubrepo.R
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val NavHostTag = "NavHostTag"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
-            val navHostFragment = NavHostFragment.create(R.navigation.nav_graph)
+            val navHostFragment = obtainNavHostFragment()
 
             supportFragmentManager.beginTransaction()
                 .add(R.id.nav_host_container, navHostFragment)
                 .setPrimaryNavigationFragment(navHostFragment)
                 .commit()
         }
+    }
+
+    private fun obtainNavHostFragment(): NavHostFragment {
+        return (supportFragmentManager.findFragmentByTag(NavHostTag) as? NavHostFragment)
+            ?: NavHostFragment.create(R.navigation.nav_graph)
     }
 }
