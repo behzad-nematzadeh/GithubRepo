feb0281c4c89ca859f7bf15239f98866020b3e02
Behzad Nematzadeh <behzad.nematzadeh@yahoo.com>
2022-05-04 22:25:50 +0430

    add splash-fragment

---
+++ app/src/main/java/ir/behzadnematzadeh/githubrepo/ui/splash/SplashFragment.kt
@@ -0,0 +1,37 @@
+package ir.behzadnematzadeh.githubrepo.ui.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import ir.behzadnematzadeh.githubrepo.R
+import ir.behzadnematzadeh.githubrepo.databinding.FragmentSpalshBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSpalshBinding? = null
+    private val binding: FragmentSpalshBinding
+        get() = requireNotNull(_binding) { "You can't touch binding when it's null" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentSpalshBinding.inflate(inflater, container, false).also {
+        _binding = it
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch{
+            delay(1000)
+            findNavController().navigate(R.id.to_repoFragment)
+        }
+    }
+}
