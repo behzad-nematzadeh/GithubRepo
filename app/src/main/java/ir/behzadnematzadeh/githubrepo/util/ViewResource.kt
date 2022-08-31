577a59cf8b503302852213a9fb1cd55abd7f490d
Behzad Nematzadeh <behzad.nematzadeh@yahoo.com>
2022-08-31 19:50:19 +0430

    add ViewResource.kt

--- app/src/main/java/ir/behzadnematzadeh/githubrepo/util/ViewResource.kt
+++ app/src/main/java/ir/behzadnematzadeh/githubrepo/util/ViewResource.kt
@@ -1,8 +1,8 @@
 package ir.behzadnematzadeh.githubrepo.util
 
 sealed interface ViewResource<T> {
-    object NotInitialized : ViewResource<Any>
-    object Loading : ViewResource<Any>
+    class NotInitialized<T> : ViewResource<T>
+    class Loading<T> : ViewResource<T>
     data class Success<T>(val data: T) : ViewResource<T>
     data class Failure<T>(val error: Throwable) : ViewResource<T>
 }
